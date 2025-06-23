ebc5476626c5088b9765bd9758629f58151e4ba2
rednikeeg <maks.schoolforvv@gmail.com>
2025-06-23 14:27:22 +0300

    Application

---
+++ src/main/java/com/rednikeeg/devqa/DevqaApplication.java
@@ -0,0 +1,13 @@
+package com.rednikeeg.devqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevqaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rednikeeg/devqa/DevqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rednikeeg.devqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
